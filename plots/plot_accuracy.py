bd42339aeeb157333e18dce9ddd141bee80df14f
Venkataramana Radhakrishnan <venkat@zendoc.com>
2019-08-07 20:34:58 +0200

    created new repo plots and adjusted test cases for algorithms

---
+++ plots/plot_accuracy.py
@@ -0,0 +1,9 @@
+# accuracies obtained from all the algorithms
+accuracy = {'MultinomialNB_tfidf': 0.5019267822736031,
+          'MultinomialNB_countvec': 0.7736030828516378,
+          'SGDClassifier_tfidf': 0.5096339113680154, 
+          'SGDClassifier_countvec': 0.7842003853564548, 
+          'LogisticRegression_tfidf': 0.5067437379576107, 
+          'LogisticRegression_countvec': 0.7822736030828517, 
+          'RandomForestClassifier_tfidf': 0.5048169556840078, 
+          'RandomForestClassifier_countvec': 0.7581888246628131}
